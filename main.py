e81031f73415ffacb6fde324d373e51dfb220d4e
Lilian Istrati <87389714+ChumpyTrucker@users.noreply.github.com>
2023-07-26 23:26:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.datasets import load_iris
 
-# Load the Iris dataset
+# Load the Iris datase
 iris = load_iris()
 X = iris.data
 y = iris.target
 
-# Split the dataset into training and testing sets
+# Split the dataset into training and testin sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Create a Support Vector Machine (SVM) classifier
 svm_classifier = SVC(kernel='linear', C=1.0, random_state=42)
 
-# Train the SVM classifier on the training data
+# Train the SVM classifier on the trainin data
 svm_classifier.fit(X_train, y_train)
 
-# Make predictions on the test data
+# Make predictions on the tes data
 y_pred = svm_classifier.predict(X_test)
 
 # Evaluate the model
