f12aef6cac30cd4e8239777cbd67fe691d02af87
Lilian Istrati <87389714+ChumpyTrucker@users.noreply.github.com>
2023-08-03 22:53:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ iris = load_iris()
 X = iris.data
 y = iris.target
 
-# Split the dataset into training and testin sets
+# Split the dataset into training and testin sets.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Create a Support Vector Machine (SVM) classifier
